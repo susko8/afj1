13da4291365349fb3621967ae1cc3d1bcdc549b9
epf.samuel.susoliak <samuel.susoliak@gmail.com>
2019-02-15 20:36:31 +0100

    file reading

---
+++ venv/freader.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+
+
+def readFile():
+    if len(sys.argv) == 2:
+        print('Argument ok,\nKompilujem subor:', str(sys.argv[1]), '\n...\n\nVystup:\n')
+    else:
+        print('Chyba nespravne spustenie skriptu: zadajte interpreter "meno suboru"')
+        sys.exit()
+    filename = str(sys.argv[1])
+    file = open(filename, 'r')
+    filecontent = file.read().splitlines()
+    return filecontent
--- venv/interpreter.py
+++ venv/interpreter.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python
 
+from freader import readFile
 
-import numpy as np
-import sys
+fileContent = readFile()
 
-# x = input('Enter your name:')
-# print('Hello, ' + x)
-
-if len(sys.argv) == 2:
-    print('Argument ok kompilujem subor:', str(sys.argv[1]))
-else:
-    print('Chyba nespravne spustenie skriptu: zadajte interpreter "meno suboru"')
-    sys.exit()
-
-fileName = str(sys.argv[1])
-print()
+print(fileContent)
